4b5bc84d35b953bb4097c72414ffd34bd45b44cc
isaac boateng <iboateng@sportingsolutions.com>
2015-01-15 10:29:05 +0000

    Fixed web errors

--- FairFlights/FairFlights.Web/App_Start/BundleConfig.cs
+++ FairFlights/FairFlights.Web/App_Start/BundleConfig.cs
@@ -8,11 +8,9 @@ namespace FairFlights.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                    ""));
+            bundles.Add(new ScriptBundle("~/bundles/corelibs").Include(
+                        "~/Scripts/Libs/jquery-{version}.js",
+                        "~/Scripts/Libs/angular.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
--- FairFlights/FairFlights.Web/Global.asax.cs
+++ FairFlights/FairFlights.Web/Global.asax.cs
@@ -16,8 +16,6 @@ namespace FairFlights.Web
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);

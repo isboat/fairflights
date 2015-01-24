265eb040b18d5630823e25f5cad20733866763f3
isboat <isboat@gmail.com>
2015-01-24 16:38:56 +0000

    add loading functionality

--- FairFlights/FairFlights.Web/Scripts/App/graphController.js
+++ FairFlights/FairFlights.Web/Scripts/App/graphController.js
@@ -58,10 +58,10 @@ ffApp.controller('graphController', function ($scope, flightService) {
             scaleFontStyle: "normal",
 
             // String - Scale label font colour
-            scaleFontColor: "#666",
+            scaleFontColor: "#fff",
 
             // Boolean - whether or not the chart should be responsive and resize when the browser does.
-            responsive: false,
+            responsive: true,
 
             // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
             maintainAspectRatio: true,
@@ -265,30 +265,28 @@ ffApp.controller('graphController', function ($scope, flightService) {
         datasets: [
             {
                 label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
                 data: [65, 59, 80, 81, 56, 55, 40]
             },
             {
                 label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
                 data: [28, 48, 40, 19, 86, 27, 90]
             }
         ]
     };
 
     var lineOptions = {
+        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+        scaleBeginAtZero: true,
 
-        ///Boolean - Whether grid lines are shown across the chart
+        //Boolean - Whether grid lines are shown across the chart
         scaleShowGridLines: true,
 
         //String - Colour of the grid lines
@@ -303,37 +301,23 @@ ffApp.controller('graphController', function ($scope, flightService) {
         //Boolean - Whether to show vertical lines (except Y axis)
         scaleShowVerticalLines: true,
 
-        //Boolean - Whether the line is curved between points
-        bezierCurve: true,
+        //Boolean - If there is a stroke on each bar
+        barShowStroke: false,
 
-        //Number - Tension of the bezier curve between points
-        bezierCurveTension: 0.4,
+        //Number - Pixel width of the bar stroke
+        barStrokeWidth: 2,
 
-        //Boolean - Whether to show a dot for each point
-        pointDot: true,
+        //Number - Spacing between each of the X value sets
+        barValueSpacing: 10,
 
-        //Number - Radius of each point dot in pixels
-        pointDotRadius: 4,
-
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth: 1,
-
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-        pointHitDetectionRadius: 20,
-
-        //Boolean - Whether to show a stroke for datasets
-        datasetStroke: true,
-
-        //Number - Pixel width of dataset stroke
-        datasetStrokeWidth: 2,
-
-        //Boolean - Whether to fill the dataset with a colour
-        datasetFill: true,
+        //Number - Spacing between data sets within X values
+        barDatasetSpacing: 1,
 
         //String - A legend template
         legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 
-    };
-    var lineChart = chart.Line(data, lineOptions);
+    }
+
+    var lineChart = chart.Bar(data, lineOptions);
 
 });
---
+++ FairFlights/FairFlights.Web/Scripts/App/uiHelper.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+var UIHelper = function () {
+
+    this.IsLoading = false;
+
+    this.ShowBusy = function (ele) {
+
+        var $el = ele || 'body';
+
+        $($el).prepend('<div class="loading"></div>');
+        $('.loading').css({
+            width: $($el).css('width'),
+            height: $($el).css('height')
+        });
+        this.IsLoading = true;
+    };
+
+    this.HideBusy = function () {
+        $('.loading').remove();
+    };
+};
+
+var uiHelper = new UIHelper();
